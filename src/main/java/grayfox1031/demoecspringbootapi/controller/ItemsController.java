94edbf59f790decafd26febba20bfc6c7bb82071
GrayFox1031 <iamgrayfox1031@gmail.com>
2021-09-23 23:48:48 +0900

    added getItemDetail to itemsController

--- src/main/java/grayfox1031/demoecspringbootapi/controller/ItemsController.java
+++ src/main/java/grayfox1031/demoecspringbootapi/controller/ItemsController.java
@@ -29,5 +29,12 @@ public class ItemsController {
         return "items";
     }
 
+    @GetMapping(value = "/itemDetail_id", produces = "application/json")
+    public String getItemDetail(Model model)  throws IOException {
 
+        model.addAttribute("genre", genreLogic.getGenre());
+//        model.addAttribute("items", itemsLogic.getItemList());
+
+        return "itemDetail";
+    }
 }
